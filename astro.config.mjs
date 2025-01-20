82379a88513755eabd300866abf0cfcf16eab60a
Anadon Raymond <raymond@anadon.eu>
2025-01-20 14:28:09 +0100

    Test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://raymondanadon.github.io',
+  base: 'raymond-portfolio',
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
